46ed4e959124e9e95bb6d5250dee1cca7afd5aa7
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2021-12-03 01:25:22 +0100

    Update train.py

--- MCTS/train.py
+++ MCTS/train.py
@@ -15,6 +15,6 @@ game = TicTacToe()
 model = Model(create_network(game.shape, game.nb_actions), summary=True)
 
 # %%
-params = {"nb_games": 20, "nb_simulations": 25, "min_win_ratio": 0.5}
+params = {"nb_games": 20, "nb_simulations": 75, "min_win_ratio": 0.51}
 model.train(game, nb_iter=30, nb_simulations=25, nb_games=100, nb_epochs=20, accept_model_params=params, plot=False)
 model.save("networks/network_tic_tac_toe.h5")
