0e7ad0110fef36a0636e5419c3d58cff3df98c47
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2022-01-06 14:00:35 +0100

    Update cartpole.py

--- DQN/cartpole.py
+++ DQN/cartpole.py
@@ -14,9 +14,10 @@ dqn_agent = DQNAgent(env, np.array([0, 1]))
 #dqn_agent.dqn.save_nn(network_dir)
 
 # Test the agent
+dqn_agent.dqn.load_nn(network_dir)
 
 total_rewards = 0
-nb_iterations = 100
+nb_iterations = 1
 for i in range(nb_iterations):
   dqn_agent.dqn.load_nn(network_dir)
   obs = env.reset()
@@ -24,7 +25,7 @@ for i in range(nb_iterations):
   steps = 0
   while not done:
     steps += 1
-    #plt.imshow(env.render(mode="rgb_array"))
+    plt.imshow(env.render(mode="rgb_array"))
     obs, reward, done, _ = env.step(dqn_agent.act(obs))
     total_rewards += reward
   print(f"({i+1}/{nb_iterations}) Total steps survived: {steps}")
